34a00c481d6ab1e5e54f214c6572c4293acd6329
blyncnov <bholuwatife00@gmail.com>
2023-07-04 18:09:40 +0100

    cake-jolt => starter code setup by blyncnov

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+import NavBarType from "@/types/types";
+
+export const Navigation_Lists: NavBarType[] = [
+  { id: 1, name: "Sign up", url: "sign-up" },
+  { id: 2, name: "Log in", url: "sign-in" },
+  { id: 3, name: "Lorem", url: "lorem" },
+];
+
+export default Navigation_Lists;
---
+++ src/types/types.d.ts
@@ -0,0 +1,7 @@
+export interface NavBarType {
+  id: number;
+  name: string;
+  url: string;
+}
+
+export default NavBarType;
